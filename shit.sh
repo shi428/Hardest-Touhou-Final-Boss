00a9b9261e4930ba57b03569f1abe3944e05ae8d
shi428 <shi428@purdue.edu>
2019-10-28 23:23:29 -0400

    gitignore

---
+++ shit.sh
@@ -0,0 +1,18 @@
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="kevin.shi2000@gmail.com"
+
+CORRECT_NAME="shi428"
+CORRECT_EMAIL="shi428@purdue.edu"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
